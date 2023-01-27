0e618d46a0be5a60d79df4d739c019c7138edf5a
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-01-27 01:44:29 -0500

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -18,19 +18,29 @@ accountFormElement.addEventListener("submit", function (event) {
     messageElement.classList.add("bg-red-300");
     messageElement.innerHTML = `<p>Error message: Name field is empty!</p>`;
   } else if (fullName.length <= 3) {
-    messageElement.innerHTML = `<p>Error message: Name is less than 3 character.</p>`;
+    messageElement.innerHTML = `<p>Error message: Name is less than 3 characters.</p>`;
   } else {
-    messageElement.innerHTML = `<p>Name: ${fullName}</p>`;
+    messageElement.innerHTML = `<p>Name: <b>${fullName}</b></p>`;
   }
 
   if (emailValue.indexOf("@") === -1) {
-    emailMessageElement.innerHTML = `<p>${emailValue}</p>`;
     console.log("Email input does not contains @");
+    emailMessageElement.innerHTML = `<p>Error message: Email address input <b>${emailValue}</b> does not contains @</p>`;
+
   } else {
     console.log("Email input contains @");
-    emailMessageElement.innerHTML = `<p>${emailValue}</p>`;
+    emailMessageElement.innerHTML = `<p>Email address input <b>${emailValue}</b> contains @</p>`;
   }
 });
+
+let demoElement = document.getElementById("demo");
+demoElement.innerHTML = `<hr><small><p>List of checked messages --></p>
+<p>- Error message: Name field is empty!</p>
+<p>- Error message: Name is less than 3 characters.</p>
+<p>- Name: <b>full-name</b></p>
+<p>- Error message: Email address input <b>email</b> does not contains '@'</p>
+<p>- Email address input <b>email</b> contains @</p></small>
+`;
 
 // use-case 2: @TODO: Add a form field that accepts a form
 // ...and if the input doesn't contain an "@"
